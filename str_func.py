c345da99154c9403fcb2d59877ef0c89ede7b418
Ralina Latypova <Latypovaralina99@gmail.com>
2023-02-17 23:03:48 -0800

    Added function to upper all words in text

--- str_func.py
+++ str_func.py
@@ -1,2 +1,14 @@
 def task2(text):
+    '''Делает заглавными всю строку'''
     return text.upper()
+
+def upper_first(text):
+    '''Делает заглавными первые буквы каждого слова в строке, поступившей на вход функции'''
+    out_text = ""
+    out_text += text[0].upper()
+    for i in range(1,len(text)):
+        if text[i-1] == " ":
+            out_text += text[i].upper()
+        else:
+            out_text += text[i]
+    return out_text
